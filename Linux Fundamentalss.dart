926f60831a3d53f116a771f37f0cb7ac0c89de20
jeevanshrma.jn@gmail.com <jeevanshrma.jn@gmail.comgit config --global user.name my>
2023-06-12 20:26:50 +0530

    Linux Fundamentalss

---
+++ Linux Fundamentalss.dart
@@ -0,0 +1,68 @@
+What is linux?
+Linux is a family of open source Unix-like operating systems based on the Linux kernel, an operating system kernel first released on September 17, 1991, by Linus Torvalds. 
+
+
+Mkdir -p india/mumbai: it helps to create a directory whith parent directory and all those child directory will be created.
+
+
+
+Working with Hardware in Linux.
+
+dmesg is a command ot display the message in linux.
+
+lspci - Perepheral component interconnect.
+lsblk - list info about block devices.
+lsmem --summary : List the memory size in the system.
+free -m : shows the total and used memory in the system.
+
+
+/ root: the top of the file system directory.
+/bin: binaries and other executable programs.
+/home: home directory
+/etc: system configuration files.
+/opt: optional or third party software.
+/tmp: temporary space typically cleared on reboot.
+/usr: user related programs.
+/var: variable datas most notably logs files.
+/cgroup: controls group hierarchy.
+/boot: files needed to boot the operating system.
+/dev: devices files typically controlled by operating system and the system administrators.
+/lib: system libraries.
+/export: shared file systems.
+/lib64: system libraries, 64bits
+/lost+found Used by the file system to store recovered files after a file system check has
+been performed.
+
+/media Used to mount removable media like CD-ROMs.
+/mnt Used to mount external file systems.
+/proc Provides information about running processes.
+/sbin System administration binaries.
+/selinux Used to display information about SELinux.
+/srv Contains data which is served by the system.
+/srv/www Web server files.
+/srv/ftp FTP files.
+/sys Used to display and sometimes configure the devices and busses known to the
+Linux kernel.
+/tmp Temporary space, typically cleared on reboot. This directory can be used by the
+OS and users alike.
+/usr User related programs, libraries, and documentation. The sub-directories in /usr
+relate to those described above and below.
+/usr/bin Binaries and other executable programs.
+/usr/lib Libraries.
+/usr/local Locally installed software that is not part of the base operating system.
+/usr/sbin System administration binaries.
+/var Variable data, most notably log files.
+/var/log Log files.
+Unix Specific Directories
+
+
+
+
+
+
+
+
+ESSENTIAL Linux Commands.
+-------------------------
+
+
